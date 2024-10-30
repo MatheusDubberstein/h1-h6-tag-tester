0a4d28c88a726ab036e2ca51b510820b7d390736
MatheusDubberstein <matheusdub.s@gmail.com>
2024-10-29 21:48:25 -0300

    update popup to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        tokyonight: {
+          red: "#F7768E",
+          green: "#9ECE6A",
+          orange: "#E0AF68",
+          blue: {
+            100: "#7DCFFF",
+            300: "#7AA2F7",
+            900: "#283457",
+          },
+          purple: "#BB9AF7",
+        },
+      },
+    },
   },
   plugins: [],
 };
